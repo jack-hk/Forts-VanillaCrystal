c8976ba642a87c8101ec0d358db36c9d5acb3e95
Jack HK <jackhughesking@gmail.com>
2024-02-03 22:42:50 +0000

    Added 2v2

---
+++ 2v2/2v2 Vanilla Crystal.lua
@@ -0,0 +1,15 @@
+RequiresMoonshot = false
+RequiresHighSeas = false
+Sandbox = true
+Skirmish = false --[[Currently no AI supported.]]--
+Multiplayer = true
+
+Symmetrical = false
+Mods = {}
+Author = L"Papa Sasquatch"
+DescLine1 = L"Vanilla (2v2)"
+
+--[[
+Map Created by Papa Sasquatch
+Crystal Enviroment by lies 
+https://steamcommunity.com/sharedfiles/filedetails/?id=2908915590&searchtext=crystal]]--
